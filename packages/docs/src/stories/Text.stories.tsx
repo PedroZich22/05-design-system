ba6315ef70cb331ac88dae70820cca6efc1d0871
Pedro Zich <zichpedro22@gmail.com>
2023-10-29 18:41:32 -0300

    feat: Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nisi, non officiis maiores ab excepturi ipsam autem iure placeat, dolor neque quo aliquid odit qui nostrum deserunt dolorum praesentium porro.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
